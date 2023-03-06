4287d52a6d791ae8e0a657b1bbdf503275b8d06b
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-03-06 20:52:39 +0530

    fix: spacing in remaining time text

--- lib/widgets/circular_timer.dart
+++ lib/widgets/circular_timer.dart
@@ -126,7 +126,7 @@ class _CircularTimerState extends State<CircularTimer>
   String format(Duration duration) {
     final minutes = duration.inMinutes.remainder(60).toString().padLeft(2, '0');
     final seconds = duration.inSeconds.remainder(60).toString().padLeft(2, '0');
-    return '$minutes:$seconds';
+    return '$minutes : $seconds';
   }
 
   @override
