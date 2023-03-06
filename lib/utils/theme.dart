22e58e2162a30daba60dddca06d942f092fdfc99
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2023-03-06 19:53:11 +0530

    chore: theme colors

---
+++ lib/utils/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// NOTE: Ideally [ThemeData] should be used to globally provide theme related values & remove dependency on inline styles (for any future re-design).
+// For now, using constant values to mock the UI.
+
+const kAppBarShadowColor = Color(0xFF675E94);
+const kTextPrimaryColor = Color(0xFFFFFFFF);
+const kTextSecondaryColor = Color(0xFF9692AC);
+const kColorSchemePrimaryColor = Color(0xFF42AF54);
+const kScaffoldBackgroundColor = Color(0xFF181622);
